23ec082bc50ac1d2d220e93e7b37bed709865bf3
unknown <gulsahgenc.gg@gmail.com>
2018-07-29 19:10:00 +0300

    Added ARIA and serviceWorker

---
+++ sw.js
@@ -0,0 +1,48 @@
+// Credits: https://developers.google.com/web/fundamentals/primers/service-workers/
+
+var cacheVersion = 'v1';
+var cacheFiles = [
+  '/',
+  '/css',
+  '/img',
+  '/js',
+  'index.html',
+  'restaurant.html'
+];
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(cacheVersion)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(cacheFiles);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
